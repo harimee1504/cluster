1eb50faf38a2ed92cdadba2ce873877b82040a55
harikrishnan <harimee1504@gmail.com>
2025-05-03 10:19:27 +0530

    basic react integration

---
+++ connector/src/App.js
@@ -0,0 +1,9 @@
+// src/App.js 
+import React from 'react'; 
+import './App.css'; 
+
+function App() { 
+  return ( <div className="App"> <h1>Hello, React in Django!</h1> </div> ); 
+} 
+
+export default App;
